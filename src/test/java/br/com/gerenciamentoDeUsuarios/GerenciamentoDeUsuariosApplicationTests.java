d1db13d19d65f0a65b13f43252725275cb1ebd72
Alan Coimbra <algcoimbra01@gmail.com>
2022-06-14 17:10:01 -0300

    Iniciando o Projeto

---
+++ src/test/java/br/com/gerenciamentoDeUsuarios/GerenciamentoDeUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gerenciamentoDeUsuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
